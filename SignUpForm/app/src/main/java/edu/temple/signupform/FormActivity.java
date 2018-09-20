0eae322525d19ccf0dc7cba7d33e01349651f7b7
Duc <dmtran02@gmail.com>
2018-09-20 16:40:38 -0400

    added button

--- SignUpForm/app/src/main/java/edu/temple/signupform/FormActivity.java
+++ SignUpForm/app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -1,13 +1,36 @@
 package edu.temple.signupform;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FormActivity extends AppCompatActivity {
+
+    Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        signUp = (Button) findViewById(R.id.signUpBtn);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchActivity();
+            }
+        });
     }
+
+    private void checkEditText(){
+        
+    }
+
+    private void launchActivity(){
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
+    }
+
 }
