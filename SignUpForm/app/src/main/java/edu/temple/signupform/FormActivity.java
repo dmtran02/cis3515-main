f5a8d9550af0c4d93e705e1d683f526c3713d270
Duc <dmtran02@gmail.com>
2018-09-21 15:30:48 -0400

    toast message added

--- SignUpForm/app/src/main/java/edu/temple/signupform/FormActivity.java
+++ SignUpForm/app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -1,14 +1,20 @@
 package edu.temple.signupform;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
     Button signUp;
+    CharSequence notAllFilled = "Fill out all of the fields to continue.";
+    CharSequence pwdNotEqual = "Both passwords are not equal.";
+    int duration = Toast.LENGTH_LONG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +25,44 @@ public class FormActivity extends AppCompatActivity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchActivity();
+                EditText nameUser = (EditText) findViewById(R.id.nameUserInput);
+                EditText emailUser = (EditText) findViewById(R.id.emailUserInput);
+                EditText pwdUser = (EditText) findViewById(R.id.password);
+                EditText pwdConfirmInput = (EditText) findViewById(R.id.pwdConfirm);
+                String name = nameUser.getText().toString().trim();
+                String email = emailUser.getText().toString().trim();
+                String pwd = pwdUser.getText().toString().trim();
+                String pwdConfirm = pwdConfirmInput.getText().toString().trim();
+                if(name.isEmpty() || name.length() == 0 || name.equals("") || name == null){
+                    Toast emptyInputError = Toast.makeText(FormActivity.this, notAllFilled, duration);
+                    emptyInputError.show();
+                }
+                else if(email.isEmpty() || email.length() == 0 || email.equals("") || email == null){
+                    Toast emptyInputError = Toast.makeText(FormActivity.this, notAllFilled, duration);
+                    emptyInputError.show();
+                }
+                else if(pwd.isEmpty() || pwd.length() == 0 || pwd.equals("") || pwd == null){
+                    Toast emptyInputError = Toast.makeText(FormActivity.this, notAllFilled, duration);
+                    emptyInputError.show();
+                }
+                else if(pwdConfirm.isEmpty() || pwdConfirm.length() == 0 || pwdConfirm.equals("") || pwdConfirm == null){
+                    Toast emptyInputUser = Toast.makeText(FormActivity.this, notAllFilled, duration);
+                    emptyInputUser.show();
+                }
+                else if(!pwd.equals(pwdConfirm)){
+                    Toast pwdEqualsError = Toast.makeText(FormActivity.this, pwdNotEqual, duration);
+                    pwdEqualsError.show();
+                }
+                else{
+                    launchActivity();
+                }
             }
         });
     }
 
-    private void checkEditText(){
-        
-    }
-
     private void launchActivity(){
         Intent intent = new Intent(this, WelcomeActivity.class);
+
         startActivity(intent);
     }
 
--- SignUpForm/app/src/main/java/edu/temple/signupform/WelcomeActivity.java
+++ SignUpForm/app/src/main/java/edu/temple/signupform/WelcomeActivity.java
@@ -2,6 +2,9 @@ package edu.temple.signupform;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        TextView welcomeText = new TextView();
     }
 }
