17e31f78e67863f5242e45f39856e05953a077e1
Duc Tran <dmtran02@Ducs-MBP.fios-router.home>
2018-10-03 00:21:13 -0400

    add project

---
+++ project-2/colorActivity/app/src/main/java/edu/temple/coloractivity/CustomAdapter.java
@@ -0,0 +1,45 @@
+package edu.temple.coloractivity;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class CustomAdapter extends BaseAdapter {
+
+    Context context;
+    String colors[];
+
+    public CustomAdapter(Context context, String[] colors){
+        this.context = context;
+        this.colors = colors;
+    }
+
+    @Override
+    public int getCount() {
+        return colors.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return colors[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        TextView colorsView = new TextView(context);
+        colorsView.setText(colors[position]);
+        colorsView.setBackgroundColor(Color.parseColor(colors[position]));
+
+        return colorsView;
+
+    }
+}
---
+++ project-2/colorActivity/app/src/main/java/edu/temple/coloractivity/PaletteActivity.java
@@ -0,0 +1,43 @@
+package edu.temple.coloractivity;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class PaletteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palette);
+
+        View colorView = findViewById(R.id.paletteView);
+
+        String colors[] = {"White", "Red", "Yellow", "Green", "Blue", "Magenta"};
+
+        CustomAdapter colorAdapter = new CustomAdapter(this, colors);
+
+        Spinner colorSpinner = (Spinner) findViewById(R.id.spinnerColor);
+
+        colorSpinner.setAdapter(colorAdapter);
+
+        AdapterView.OnItemSelectedListener colorOISL = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                view.setBackgroundColor(Color.parseColor(((TextView)view).getText().toString()));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+
+        colorSpinner.setOnItemSelectedListener(colorOISL);
+
+    }
+}
